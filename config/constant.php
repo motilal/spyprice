a534025b2f1b6917d5db2ebd1b52aa25b1c54c8a
Motilal Soni <motilalsoni@gmail.com>
2018-04-19 08:13:36 +0530

    constant value change

--- config/constant.php
+++ config/constant.php
@@ -1,4 +1,4 @@
 <?php
 
 define('PAGINATION_LIMIT', 8);
-define('BASE_PATH', '/mm/spyprice/'); /* give folder name of project here '/project_folder/'  if root then '/' */
+define('BASE_PATH', '/spyprice/'); /* give folder name of project here '/project_folder/'  if root then '/' */
